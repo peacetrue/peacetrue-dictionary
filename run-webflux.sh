3c8047e3e4a2bf6e98f3e7a3c72f4274dc0b8657
peacetrue <xiayouxue@gmail.com>
2022-10-24 21:27:01 +0800

    实现 reactor 技术栈

--- run-webflux.sh
+++ run-webflux.sh
@@ -8,9 +8,9 @@ cd peacetrue-application || exit
 tee peacetrue-application-webflux/extend.gradle <<EOF
 dependencies {
   // 服务实现
-  implementation "com.github.peacetrue.dictionary:${app_name}-service-impl:${app_version}"
+  implementation "com.github.peacetrue.dictionary:${app_name}-service-impl-reactor:${app_version}"
   // 控制器
-  implementation "com.github.peacetrue.dictionary:${app_name}-controller:${app_version}"
+  implementation "com.github.peacetrue.dictionary:${app_name}-controller-reactor:${app_version}"
 }
 EOF
 
