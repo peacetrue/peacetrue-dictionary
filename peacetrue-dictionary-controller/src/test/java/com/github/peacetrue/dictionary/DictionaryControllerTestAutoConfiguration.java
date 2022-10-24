ef928cc35f0f74b971c8f48236df8f8c8a695618
peacetrue <xiayouxue@gmail.com>
2022-10-24 18:51:24 +0800

    添加 workflows

--- peacetrue-dictionary-controller/src/test/java/com/github/peacetrue/dictionary/DictionaryControllerTestAutoConfiguration.java
+++ peacetrue-dictionary-controller/src/test/java/com/github/peacetrue/dictionary/DictionaryControllerTestAutoConfiguration.java
@@ -1,11 +1,16 @@
 package com.github.peacetrue.dictionary;
 
+import com.github.peacetrue.dictionary.modules.dictionarytype.DictionaryTypeService;
+import com.github.peacetrue.dictionary.modules.dictionaryvalue.DictionaryValueService;
+import org.mockito.Mockito;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
 import org.springframework.boot.autoconfigure.http.HttpMessageConvertersAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.web.ReactivePageableHandlerMethodArgumentResolver;
 import org.springframework.data.web.ReactiveSortHandlerMethodArgumentResolver;
 
@@ -14,7 +19,6 @@ import org.springframework.data.web.ReactiveSortHandlerMethodArgumentResolver;
  */
 @Configuration
 @ImportAutoConfiguration(classes = {
-        DictionaryServiceTestAutoConfiguration.class,
         WebMvcAutoConfiguration.class,
         HttpMessageConvertersAutoConfiguration.class,
         DictionaryControllerAutoConfiguration.class,
@@ -25,5 +29,16 @@ import org.springframework.data.web.ReactiveSortHandlerMethodArgumentResolver;
 @EnableAutoConfiguration
 public class DictionaryControllerTestAutoConfiguration {
 
+    @Bean
+    @Primary
+    public DictionaryTypeService dictionaryTypeService() {
+        return Mockito.mock(DictionaryTypeService.class);
+    }
+
+    @Bean
+    @Primary
+    public DictionaryValueService dictionaryValueService() {
+        return Mockito.mock(DictionaryValueService.class);
+    }
 
 }
